59c89ac5d3df11415a84eb1748d463092ca78580
Tim Willebrands <mail@timwillebrands.nl>
2024-12-07 21:10:06 +0100

    We did it chat!

--- 6/solution.fsx
+++ 6/solution.fsx
@@ -1,3 +1,4 @@
+open System.Diagnostics
 open System.IO
 
 let input =
@@ -15,21 +16,53 @@ let guard =
     |> (fun i -> (i % len, (i / len)))
 let dirs =   [|(0,-1); (1,0); (0,1); (-1,0)|]
 
-let tryGet y x =
-    if x >= 0 && x < Array2D.length1 grid && y >= 0 && y < Array2D.length2 grid then
+let tryGet y x obstruction =
+    if obstruction = (x, y) then
+        Some '#'
+    elif x >= 0 && x < Array2D.length1 grid && y >= 0 && y < Array2D.length2 grid then
         Some grid[y, x]
     else
         None
 
-let rec move dir (gx, gy) steps = 
-    let dx, dy = dirs[dir % 4]
-    let target = (dx + gx, dy + gy)
-    match tryGet (snd target) (fst target) with
-    | None -> steps
-    | Some '#' -> move (dir + 1) (gx, gy) steps
-    | _ -> move dir target (Set.add target steps)
+let rec move obstruction (gx, gy, dir) steps =
+    let dx, dy = dirs[dir]
+    let target = (dx + gx, dy + gy, dir)
+    let tx, ty, _ = target
+    match tryGet ty tx obstruction with
+    | None -> Some steps
+    | Some '#' -> move obstruction (gx, gy, (dir + 1)  % 4) steps
+    | _ ->
+        if Set.contains target steps then
+            None // loop detected!!
+        else 
+            move obstruction target (Set.add target steps)
+            
+let start = (fst guard, snd guard, 0)
+
+let findLoopPositions (originalPath: Set<int*int*int>) =
+    originalPath 
+    |> Set.toArray 
+    |> Array.Parallel.choose (fun (x, y, _) ->
+        if grid[y,x] = '.' then
+            match move (x, y) start (Set.singleton start) with
+            | None -> Some (x,y)
+            | Some _ -> None
+        else
+            None )
+    |> Set.ofArray
     
+let sw = Stopwatch.StartNew()
+let originalPath =
+    match move (-1, -1) start (Set.singleton start) with
+    | Some path -> path
+    | None -> Set.empty
+let day1 = originalPath |> Set.map (fun (x,y,_) -> (x,y)) |> Set.count
+let day1ms = sw.ElapsedMilliseconds
 
-let day1 = move 0 guard (Set.singleton guard)
+sw.Restart()
+let day2 = findLoopPositions originalPath |> Set.count
+let day2ms = sw.ElapsedMilliseconds
 
-printfn "Day1: %A" (day1 |> Set.count) // + 1?
+
+printfn "Day1: %A, calc in %i" day1 day1ms
+printfn "Day2: %A, calc in %i" day2 day2ms
